2cd3c8baab41f06cf44bc0872591d7a7e2bf3094
Aryan yadav <165578262+aryxn-builds@users.noreply.github.com>
2026-09-16 09:52:37 +0530

    Time: 3 ms (20.64%), Space: 19.4 MB (23.82%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -3,7 +3,6 @@ class Solution:
         MOD = 10**9 +7
 
         def pow(x,n):
-           
             res=1
             while n>0:
                 if n%2:
@@ -13,4 +12,4 @@ class Solution:
             return res
         even = ceil(n/2)
         odd= n//2
-        return (pow(5,even) * pow(4,odd)) %MOD
+        return (pow(5,even) * pow(4,odd)) % MOD
